c9d9b37c3b850f34b4e5283e45b433afb919cea8
TetiankaSh <shh.tetiana@gmail.com>
2025-09-20 15:00:36 +0300

    checking if there are more comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import './App.scss';
 
-// import postsFromServer from './api/posts.json';
-// import commentsFromServer from './api/comments.json';
-// import usersFromServer from './api/users.json';
+import postsFromServer from './api/posts.json';
+import commentsFromServer from './api/comments.json';
+import usersFromServer from './api/users.json';
 // import { CommentInfo } from './components/CommentInfo/CommentInfo';
 import { CommentList } from './components/CommentList/CommentList';
 import { PostInfo } from './components/PostInfo/PostInfo';
+
+const preparedPosts = postsFromServer.map(post => {
+  const user = usersFromServer.find(u => u.id === post.userId) || null;
+  const comments = commentsFromServer.filter(c => c.postId === post.id);
+
+  return { ...post, user, comments };
+});
 
 export const App = () => (
   <section className="App">
     <h1 className="App__title">Static list of posts</h1>
 
-    <div className="PostList">
-      <PostInfo />
-
-      <CommentList />
-    </div>
+    {preparedPosts.map(post => (
+      <div key={post.id} className="PostWrapper">
+        <PostInfo post={post} />
+        <CommentList comments={post.comments} />
+      </div>
+    ))}
   </section>
 );
